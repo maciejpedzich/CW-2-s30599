ce0c08fb06ca8d8ec9807f7064df0604375aff49
Maciej Pędzich <contact@maciejpedzi.ch>
2025-03-22 15:08:27 +0100

    Dodaj kontener na gaz

---
+++ CW-2-s30599/KontenerNaGaz.cs
@@ -0,0 +1,28 @@
+namespace CW_2_s30599;
+
+public class KontenerNaGazKontener(
+    uint maksLadownoscKg,
+    uint masaTaraKg,
+    uint wysokoscCm,
+    uint glebokoscCm
+) : Kontener(
+    'G',
+    maksLadownoscKg,
+    masaTaraKg,
+    wysokoscCm,
+    glebokoscCm
+),
+IHazardNotifier
+{
+    public override void OproznijLadunek()
+    {
+        MasaNettoKg /= 20;
+    }
+    
+    public void PowiadomONiebezpiecznejSytuacji()
+    {
+        Console.WriteLine(
+            $"Wykonujesz niebezpieczną czynność z kontenerem na gaz {NumerSeryjny()}!"
+        );
+    }
+}
